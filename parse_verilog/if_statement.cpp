47de2f8929d9f2f4cecba571baa19dd2e57886c1
Ned Bingham <edward.bingham@broccolimicro.io>
2026-06-30 21:52:06 -0400

    fixing spacing, making it a bit more robust

--- parse_verilog/if_statement.cpp
+++ parse_verilog/if_statement.cpp
@@ -113,14 +113,14 @@ string if_statement::to_string(string tab) const {
 	
 	for (int i = 0; i < (int)body.size(); i++) {
 		if (i != 0) {
-			if (body[i-1].sub.size() == 1u) {
+			/*if (body[i-1].sub.size() == 1u) {
 				result += tab;
 			} else {
 				result += " ";
-			}
-			result += "else";
+			}*/
+			result += " else";
 		}
-		if (i < (int)condition.size()) {
+		if (i < (int)condition.size() and condition[i].valid) {
 			if (i != 0) {
 				result += " ";
 			}
